afb87c0560ba35e71543cb3ba3c4fe51f3e6dfb4
Jesús Andrés Mata <andresmata.c21@gmail.com>
2023-01-15 00:56:19 -0600

    Testing the splashscreen and the new icon

--- android/app/src/main/java/com/navegacionapp/MainActivity.java
+++ android/app/src/main/java/com/navegacionapp/MainActivity.java
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import android.os.Bundle;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -18,7 +20,8 @@ public class MainActivity extends ReactActivity {
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
